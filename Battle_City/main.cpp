5f729d6c9d22bbe8075ebd47d57e27eaafa70e7f
mingzhip <pmz1321@163.com>
2013-04-09 11:40:08 +0800

    test hge

--- Battle_City/main.cpp
+++ Battle_City/main.cpp
@@ -1,10 +1,20 @@
 ﻿#include <hge.h>
+#include <hgesprite.h>
 
 HGE *hge=0;
+HTEXTURE bgtex;
+hgeSprite *bgspr;
+
 bool RenderFunc()
 {
+    hge->Gfx_BeginScene();
+
+    bgspr->Render(0,0);
+
+    hge->Gfx_EndScene();
     return false;
 }
+
 bool FrameFunc()
 {
     return false;
@@ -15,15 +25,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
     hge=hgeCreate(HGE_VERSION);
     hge->System_SetState(HGE_SCREENWIDTH, 800);
     hge->System_SetState(HGE_SCREENHEIGHT,600);
+    hge->System_SetState(HGE_SHOWSPLASH, false);
     hge->System_SetState(HGE_FRAMEFUNC, FrameFunc);
     hge->System_SetState(HGE_RENDERFUNC,RenderFunc);
+    hge->System_SetState(HGE_FPS,30);
     hge->System_SetState(HGE_TITLE, "hello world");
     hge->System_SetState(HGE_WINDOWED,true);
     hge->System_SetState(HGE_USESOUND,false);
 
     if(hge->System_Initiate())
     {
+        bgtex=hge->Texture_Load("bg.png");
+        bgspr=new hgeSprite(bgtex,0,0,800,600);
+        
         hge->System_Start();
+        
+        delete bgspr;
+        hge->Texture_Free(bgtex);
     }
 
     hge->System_Shutdown();
---
+++ Battle_City/res.h
@@ -0,0 +1,8 @@
+#include <hge.h>
+#include <hgeresource.h>
+
+void loadres(hgeResourceManager *resmanager)
+{
+    resmanager = new hgeResourceManager("res.config");
+    
+}
