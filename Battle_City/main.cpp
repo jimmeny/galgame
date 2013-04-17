21db57b14d2b88976ec006dbfd6918435bd21246
jian yang <648658759@qq.com>
2013-04-17 16:43:58 +0800

    enu change

--- Battle_City/main.cpp
+++ Battle_City/main.cpp
@@ -86,7 +86,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		gui=new hgeGUI();
 
 		gui->AddCtrl(new MenuItem(1,400,200,0.0f,L"スタート"));
-		gui->AddCtrl(new MenuItem(2,400,240,0.1f,L"続き"));
+		gui->AddCtrl(new MenuItem(2,400,240,0.1f,L"ロード"));
 		gui->AddCtrl(new MenuItem(3,400,280,0.2f,L"オプション"));
 		gui->AddCtrl(new MenuItem(4,400,320,0.3f,L"説明"));
 		gui->AddCtrl(new MenuItem(5,400,360,0.4f,L"終了"));
